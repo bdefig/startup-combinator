cd6fbc55291947dc35bff6a4b773c37a9a316995
Bryce DeFigueiredo <bdefig@gmail.com>
2018-02-10 09:05:03 -0700

    'Another' button works

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ let nouns = require('./Nouns');
 let businessModels = require('./BusinessModels');
 
 class App extends Component {
+  handleAnother = () => {
+    this.forceUpdate();
+  }
+
   render() {
     const randomThing = (things) => {
       return things[Math.floor(Math.random() * things.length)];
@@ -16,12 +20,14 @@ class App extends Component {
         <div className='App-titleWrapper'>
           <h1 className='App-title'>Startup Combinator</h1>
         </div>
-        <h2>{randomThing(businessModels) + ' for ' + pluralize(randomThing(nouns)) + ' 💸'}</h2>
-        <div className='App-rateWrapper'>
-          <div className='App-rateThumb'>👍</div>
-          <div className='App-rateThumb'>👎</div>
+        <div className='App-theMagic'>
+          {randomThing(businessModels) + ' for ' + pluralize(randomThing(nouns)) + ' 💸'}
         </div>
-        <div className='App-another' onClick={this.forceUpdate}>
+        <div className='App-rateWrapper'>
+          <div className='App-rateThumb'>👎</div>
+          <div className='App-rateThumb'>👍</div>
+        </div>
+        <div className='App-another' onClick={() => this.handleAnother()}>
           Another!
         </div>
       </div>
