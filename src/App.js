913b0f8a46688726ca3003f613e092e4393dfa5d
Bryce DeFigueiredo <bdefig@gmail.com>
2018-02-09 17:59:05 -0700

    Got some nice styles and randomness going on

--- src/App.js
+++ src/App.js
@@ -3,29 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 let pluralize = require('pluralize');
 let nouns = require('./Nouns');
-
-const randomNoun = (words) => {
-  return words[Math.floor(Math.random() * words.length)];
-}
+let businessModels = require('./BusinessModels');
 
 class App extends Component {
   render() {
+    const randomThing = (things) => {
+      return things[Math.floor(Math.random() * things.length)];
+    }
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          {/* {pluralize('index')} */}
-          {pluralize(randomNoun(nouns))}
-        </p>
+      <div className='App'>
+        <div className='App-titleWrapper'>
+          <h1 className='App-title'>Startup Combinator</h1>
+        </div>
+        <h2>{randomThing(businessModels) + ' for ' + pluralize(randomThing(nouns)) + ' 💸'}</h2>
+        <div className='App-rateWrapper'>
+          <div className='App-rateThumb'>👍</div>
+          <div className='App-rateThumb'>👎</div>
+        </div>
+        <div className='App-another' onClick={this.forceUpdate}>
+          Another!
+        </div>
       </div>
+      
     );
   }
 }
 
 export default App;
+
+{/* <div className="App">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">Welcome to React</h1>
+  </header>
+  <p className="App-intro">
+    To get started, edit <code>src/App.js</code> and save to reload.
+  </p>
+  <p>
+    {pluralize(randomNoun(nouns))}
+  </p>
+</div> */}
---
+++ src/BusinessModels.js
@@ -0,0 +1,5 @@
+module.exports = [
+    'Uber',
+    'Airbnb',
+    'Tinder'
+]
